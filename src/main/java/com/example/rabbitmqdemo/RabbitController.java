94d1fc7cb291045e4a3008c015e266016745f28a
Linus <git@linus9000.de>
2024-06-26 15:56:50 +0200

    try asynchronous publisher confirms

--- src/main/java/com/example/rabbitmqdemo/RabbitController.java
+++ src/main/java/com/example/rabbitmqdemo/RabbitController.java
@@ -2,6 +2,7 @@ package com.example.rabbitmqdemo;
 
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConfirmCallback;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import lombok.extern.apachecommons.CommonsLog;
@@ -11,16 +12,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 
 @RestController
 @RequestMapping("/")
 @CommonsLog
 public class RabbitController {
+
+
+    private static final String EXCHANGE_NAME = "myexchange";
+    private static final String DESTINATION_QUEUE_NAME = "myqueue";
+    private static final String ROUTING_KEY = "myrouting";
 
 
     @GetMapping
@@ -36,24 +43,48 @@ public class RabbitController {
 
         String status = "";
 
+        ConcurrentNavigableMap<Long, String> outstandingConfirms = new ConcurrentSkipListMap<>();
+        ConfirmCallback cleanOutstandingConfirms = (sequenceNumber, multiple) -> {
+            if (multiple) {
+                ConcurrentNavigableMap<Long, String> confirmed = outstandingConfirms.headMap(
+                        sequenceNumber, true
+                );
+                confirmed.clear();
+            } else {
+                outstandingConfirms.remove(sequenceNumber);
+            }
+        };
+
         try (Connection conn = factory.newConnection()) {
 
             Channel channel = conn.createChannel();
 
             channel.confirmSelect();
-            channel.exchangeDeclare("myexchange", "direct", true);
-            channel.queueDeclare("myqueue", true, false, false, Map.of("x-queue-type", "quorum"));
-            channel.queueBind("myqueue", "myexchange", "myrouting");
+            channel.addConfirmListener(cleanOutstandingConfirms, (sequenceNumber, multiple) -> {
+                String body = outstandingConfirms.get(sequenceNumber);
+                log.error(String.format(
+                        "Message with body %s has been nack-ed. Sequence number: %d, multiple: %b%n",
+                        body, sequenceNumber, multiple)
+                );
+                cleanOutstandingConfirms.handle(sequenceNumber, multiple);
+            });
+            channel.exchangeDeclare(EXCHANGE_NAME, "direct", true);
+            channel.queueDeclare(DESTINATION_QUEUE_NAME, true, false, false, Map.of("x-queue-type", "quorum"));
+            channel.queueBind(DESTINATION_QUEUE_NAME, EXCHANGE_NAME, ROUTING_KEY);
 
             log.info("Sending %s messages...".formatted(count));
 
             Set<Integer> sentMessages = new HashSet<>(count);
             Set<Integer> failedMessages = new HashSet<>();
+
+
             for (int i = 0; i < count; i++) {
                 try {
-                    this.sendMessage(channel, "myexchange", "myrouting", String.valueOf(i));
+                    String body = String.valueOf(i);
+                    outstandingConfirms.put(channel.getNextPublishSeqNo(), body);
+                    channel.basicPublish(EXCHANGE_NAME, ROUTING_KEY, true, new AMQP.BasicProperties.Builder().deliveryMode(2).build(), body.getBytes());
                     sentMessages.add(i);
-                } catch (MessageSendException e) {
+                } catch (Exception e) {
                     log.error("Could not send message with id " + i, e);
                     failedMessages.add(i);
                 }
@@ -66,24 +97,9 @@ public class RabbitController {
         }
 
 
-
-
         log.info(status);
 
         return ResponseEntity.ok(status);
-    }
-
-
-    private void sendMessage(Channel channel, String exchange, String routingKey, Object content) throws MessageSendException{
-
-        try {
-            channel.basicPublish(exchange, routingKey, true, new AMQP.BasicProperties.Builder().deliveryMode(2).build(), content.toString().getBytes());
-            channel.waitForConfirmsOrDie(30_000);
-        } catch (Exception e) {
-            throw new MessageSendException(e);
-
-        }
-
     }
 
 }
